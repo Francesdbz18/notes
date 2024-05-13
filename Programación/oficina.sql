58e71f3b784f1d5c81a1de95a24383574b527e68
Francesco <francesdbz18pc@outlook.es>
2024-05-13 23:14:44 +0200

    Apuntes

--- Programación/oficina.sql
+++ Programación/oficina.sql
@@ -26,9 +26,9 @@ insert into empleados values ('Juan','Torres', '1960-01-01', '1980-05-24', 'Jefe
 insert into empleados values('Sara','González','1980-05-02','1999-06-03','Secretaria',25000.0);
 insert into empleados values('Elena','Sánchez','1990-09-03','2010-11-02','Técnico FP',30000.0);
 insert into empleados values('Pepe','Uriel','1991-10-04','2015-10-01','Administrativo',24000.0);
--- insertar desde empleados.csv -- 
+-- alt: insertar desde empleados.csv -- 
 
 -- create user 'bdAdmin'@'192.168.80.24' identified by 'BDAdmin1!'; --
 #grant all privileges on oficina.* to 'bdAdmin'@'192.168.80.24';
-#flush privileges;
+#flush privileges
 
