0b219325310d153a33655f7511845b65e57e2c90
Francesco <francesdbz18pc@outlook.es>
2024-05-14 10:40:20 +0200

    Apuntes

--- Programación/oficina.sql
+++ Programación/oficina.sql
@@ -22,13 +22,10 @@ create table if not exists empleadosAntiguos (
 	salario double,
 	fechaSalida date
 	);
-insert into empleados values ('Juan','Torres', '1960-01-01', '1980-05-24', 'Jefe', 60000.0);
-insert into empleados values('Sara','González','1980-05-02','1999-06-03','Secretaria',25000.0);
-insert into empleados values('Elena','Sánchez','1990-09-03','2010-11-02','Técnico FP',30000.0);
-insert into empleados values('Pepe','Uriel','1991-10-04','2015-10-01','Administrativo',24000.0);
--- alt: insertar desde empleados.csv -- 
 
--- create user 'bdAdmin'@'192.168.80.24' identified by 'BDAdmin1!'; --
-#grant all privileges on oficina.* to 'bdAdmin'@'192.168.80.24';
-#flush privileges
+-- insertar desde empleados.csv -- 
+
+create user 'bdadmin'@'192.168.80.24' identified by 'BDAdmin1!';
+grant all privileges on oficina.* to 'bdadmin'@'192.168.80.24';
+flush privileges;
 
